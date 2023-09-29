b81f05fc02e4c0b2e1ce703795360fd48ede5d4e
IrishDante <46965975+IrishDante@users.noreply.github.com>
2023-09-29 13:17:53 +0100

    Update script.js
    removed test JS that wasn't working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,1 @@
-var buttons[
-    {'name': "Rock", 'beats':{"Scissors": 'crushes', "Lizard": 'crushes'}},
-    {'name': "Paper", 'beats': { "Rock": 'covers', "Spock":'disproves'}},
-    {'name': "Scissors", 'beats': {"Paper":'cuts',":Lizard":'decapitates'}},
-    {'name': "Lizard", 'beats': {"Spock": 'poisons', "Paper": 'eats'}},
-    {'name': "Spock", 'beats': { "Scissors": 'smashes', "Rock": 'vaporises'} },
-];
 
-for(bttn of buttons){
-    let button = document.createElement('button');
-    button.appendChild(document.createTextNode(bttn.name))
-    document.body.getElementById('rpslsGame').appendChild(button);
-    button.addEventListener('click', function() {choice(bttn);});
-}
