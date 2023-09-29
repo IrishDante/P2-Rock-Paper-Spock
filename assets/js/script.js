5da38dfa1268bd8d557e2946ee537f066e85f1bd
IrishDante <46965975+IrishDante@users.noreply.github.com>
2023-09-29 17:03:32 +0100

    Update script.js
    fixed typo in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-/*declare constants for DOM elements*/
+//declare constants for DOM elements
 const buttons = document.getElementsByClassName("control");
 const playerScore = document.getElementById("player-score");
 const opponentScore = document.getElementById("opponent-score");
 const gameChoices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
-/*add event listener to all buttons*/
+//add event listener to all buttons
 for(let button of buttons){
   button.addEventListener("click", function(){
     let playerChoice = this.getAttribute("data-type")'
     playGame(playerChoice);
+  })
 }
 
-/*this function compares the player choice and the computer choice to see who wins and updates the scores accordingly*/
 function playGame(playerChoice){
+  
   let opponentChoice = gameChoices[Math.floor(Math.random()*5)];
 
   if (playerChoice == opponentChoice){
