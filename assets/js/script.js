d538e6e556f07d9d926526a41678614dca82879c
IrishDante <46965975+IrishDante@users.noreply.github.com>
2023-09-29 16:26:50 +0100

    Update script.js
    start work on js added constants, assign listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+/*declare constants for DOM elements*/
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const opponentScore = document.getElementById("opponent-score");
 
+/*add event listener to all buttons*/
+for(let button of buttons){
+  button.addEventListener("click", function(){
+    let playerChoice = this.getAttribute("data-type")'
+    playGame(playerChoice);
+}
+
+/*this function compares the player choice and the computer choice to see who wins and updates the scores accordingly*/
+function playGame(playerChoice){
+  
+}
